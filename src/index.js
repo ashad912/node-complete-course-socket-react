355d72c2a190d3e78a0ead9830ba25f1f687661a
ashad912 <krzysztof.los912@gmail.com>
2019-09-18 17:44:26 +0200

    socket strategy view + funcs

--- src/index.js
+++ src/index.js
@@ -1,37 +1,9 @@
 import {app} from './app'
-import socket from 'socket.io'
 
 //refactor needed cause we want to test before listenin
 //listen for reqs
 
-const server = app.listen(process.env.PORT || 4000, ()=>{ //process.env.port <- listening to setup hosting variable
+export const server = app.listen(process.env.PORT || 4000, ()=>{ //process.env.port <- listening to setup hosting variable
     console.log('now listenin for reqs')
 })
 
-var io = socket(server) //param is a server, defined upper
-
-io.of('/dnd').on('connection', (socket) => { //each client has own socket
-    console.log('New client connected', socket.id)
-
-    socket.on('test', (data)=>{ //data is an object sent by client side
-        console.log('got mssg')
-        io.of('dnd').emit('test', data) // 'sockets' - all sockets connected ll see it
-    })
-
-    socket.on('updateItems', (items)=>{ //data is an object sent by client side
-        console.log('got items')
-        io.of('dnd').emit('updateItems', items) // 'sockets' - all sockets connected ll see it
-    })
-
-    socket.on('addItem', (item) => {
-        console.log('add item server')
-        io.of('dnd').emit('addItem', item)
-    })
-
-    socket.on('deleteItem', (id) => {
-        console.log('delete item server')
-        io.of('dnd').emit('deleteItem', id)
-    })
-
-    socket.on("disconnect", () => console.log("Client disconnected"));
-})
